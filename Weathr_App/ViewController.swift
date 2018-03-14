1ce2fa8458f0d83866c61602c87518042e808b71
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-03-14 11:29:40 +0100

    working app

--- Weathr_App/ViewController.swift
+++ Weathr_App/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCity: UILabel!
+    @IBOutlet weak var lblTemp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=s-Hertogenbosch&appid=3b7c0bb2df5778f696d6dfc53b6189c9")!
+        
+        let task = URLSession.shared.dataTask(with: url) {data, response, error in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! [String: Any]
+                
+                let main = json["main"] as! [String: Any]
+                let kelvin_temparature = main["temp"] as! Double
+                let celcius = kelvin_temparature - 272.15
+                
+                DispatchQueue.main.async {
+                    self.lblTemp.text = "\(String(format: "%.if", celcius))"
+                }
+            } catch {
+                
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
